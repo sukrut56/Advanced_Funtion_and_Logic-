d8cd6ad3eb9e06d785b59d6b261c16853b332082
Sukrut <37614346+sukrut56@users.noreply.github.com>
2022-09-25 18:30:43 -0600

    Update function_filter_list.py

--- function_filter_list.py
+++ function_filter_list.py
@@ -1,6 +1,6 @@
-def three (arg):						      #defining a function which taken an argument 
-	my_list = [1,2,3,4,5,6,7,8,9]		#list which will be used 
-	g = [x for x in my_list if x<= arg]	#using list comprehension to count the list till specified argument only 
-	print(g)							          #printing the list 
+def three (arg):					#defining a function which taken an argument 
+	my_list = [1,2,3,4,5,6,7,8,9]			#list which will be used 
+	list_comp = [x for x in my_list if x<= arg]	#using list comprehension to count the list till specified argument only 
+	print(list_comp)				#printing the list 
 
-three(5)								          #specifying the argument for the function 
+three(5)						#specifying the argument for the function 
