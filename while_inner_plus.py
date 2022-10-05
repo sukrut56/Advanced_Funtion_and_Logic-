c18aacdaee175e64be0b31ad22f0c130c658ab8b
BMIJacksonViscomi <112350037+BMIJacksonViscomi@users.noreply.github.com>
2022-10-04 19:51:40 -0600

    Update while_inner_plus.py

--- while_inner_plus.py
+++ while_inner_plus.py
@@ -1,17 +1,22 @@
-input_list = [1,2,3,4,[5,6,7,[8,9,]]]
-
-list1 = input_list
-ind = 0
-
-while True:
-  length = len(list1)           #assigning variable the length of list 
-  if type(list1[ind]) == list:  #comparing if both the types are same 
-    list1 = list1[ind]          #using indexing, we assign the value to list 
-    ind = 0                     #assigning ind the value of 0 so that the counting starts from 0 
-  else:
-    ind += 1                    #iterating the value of ind by 1  
-  if ind == length:             #if ind is same as length of the list, then only it will break 
-  	break
-
-add = [x+1 for x in list1]      #using list comprehension to add 1 to the list
-print(add)
+#Q1
+def q1(lst):
+    while any(type(x) == list for x in lst): #if any of the elements in the list are also lists (nested) then enter loop
+        x = 0 #start iterator
+        replace_list = [] #new list for nested lists
+        while x < len(lst): 
+            if type(lst[x]) == list: #if that element is a list
+                y = 0 #new iterator for nested lists
+                while y < len(lst[x]):
+                    replace_list.append(lst[x][y]) #apend nested lists to new list 
+                    y += 1 #iterate through nested list
+                x += 1 #iterate through list
+            else:
+                x += 1 #do nothing and iterate through list
+        lst = replace_list.copy() #chaning list to list of nested lists       
+    z = 0 #once out of loop, start counter for adding 1
+    while z < len(lst):
+        lst[z] += 1 #adding one to each element in final list
+        z += 1
+    return lst
+input_list = [1,2,[3,4],5,6,7,[8,[9,10]],11]
+q1(input_list)
